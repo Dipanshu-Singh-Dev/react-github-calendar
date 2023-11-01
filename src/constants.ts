a9bd91caea6bc6cf732295f876a78a928a7d3e02
Dipanshu-Singh-Dev <work.dipanshu.singh@gmail.com>
2023-11-01 20:03:03 +0530

    updated colors

--- src/constants.ts
+++ src/constants.ts
@@ -5,5 +5,5 @@ export const API_URL = 'https://github-contributions-api.jogruber.de/v4/';
 // GitHub theme
 export const DEFAULT_THEME: ThemeInput = {
   light: ['#ebedf0', '#9be9a8', '#40c463', '#30a14e', '#216e39'],
-  dark: ['#161b22', '#0e4429', '#006d32', '#26a641', '#39d353'],
+  dark: ['#2b2b2b', '#595959', '#b3b3b3', '#d9d9d9', '#f2f2f2']
 };
